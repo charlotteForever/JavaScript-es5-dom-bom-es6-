542e0d8a13b048b93e1598797f221d86cc98cb3f
charlotteForever <fernweh1001@163.com>
2022-01-19 15:50:03 +0800

    手写promise_实现resolve和reject

--- 5_promise/3_手写promise/promise.js
+++ 5_promise/3_手写promise/promise.js
@@ -1,5 +1,22 @@
 function Promise(excutor) {
+    this.promiseState = 'pending'
+    this.promiseResult = null
+    const self = this
 
+    function resolve(data) {
+        // 修改状态为fulfilled
+        self.promiseState = 'fulfilled'
+        // 修改result为data
+        self.promiseResult = data
+    }
+    function reject(data) {
+        // 修改状态为fulfilled
+        self.promiseState = 'rejected'
+        // 修改result为data
+        self.promiseResult = data
+    }
+    // 同步调用执行器函数
+    excutor(resolve, reject)
 }
 Promise.prototype.then = function (onResolve, onReject) {
 
