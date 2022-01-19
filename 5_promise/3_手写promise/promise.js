52c219cdbecc393793392f7c9d10c2684465a7fb
charlotteForever <fernweh1001@163.com>
2022-01-19 20:13:22 +0800

    手写promise_throw改变promise状态&then回调&promise状态只能修改一次

--- 5_promise/3_手写promise/promise.js
+++ 5_promise/3_手写promise/promise.js
@@ -4,20 +4,39 @@ function Promise(excutor) {
     const self = this
 
     function resolve(data) {
-        // 修改状态为fulfilled
-        self.promiseState = 'fulfilled'
-        // 修改result为data
-        self.promiseResult = data
+        // promise对象的状态只能修改一次
+        if (self.promiseState !== 'pending') {
+            // 修改状态为fulfilled
+            self.promiseState = 'fulfilled'
+            // 修改result为data
+            self.promiseResult = data
+        }
+
     }
     function reject(data) {
-        // 修改状态为fulfilled
-        self.promiseState = 'rejected'
-        // 修改result为data
-        self.promiseResult = data
+        if (self.promiseState !== 'pending') {
+            // 修改状态为rejected
+            self.promiseState = 'rejected'
+            // 修改result为data
+            self.promiseResult = data
+        }
     }
-    // 同步调用执行器函数
-    excutor(resolve, reject)
+    try {
+        // 同步调用执行器函数
+        excutor(resolve, reject)
+    } catch (e) {
+        // 抛出异常时，修改状态为rejected
+        // self.promiseState = 'rejected'
+        // self.promiseResult=
+        reject(e)
+    }
+
 }
 Promise.prototype.then = function (onResolve, onReject) {
-
+    // 接收一个onResolve和onReject的函数体，判断之后把参数值传入就OK
+    if (this.promiseState === 'fulfilled') {
+        onResolve(this.promiseResult)
+    } else {
+        onReject(this.promiseResult)
+    }
 }
